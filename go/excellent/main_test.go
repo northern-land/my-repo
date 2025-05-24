641f21708ea57fd7760837bc35236fd7f9d8f72b
northis <northis@icloud.com>
2025-05-24 17:27:54 +0900

    Fix main_test.go

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
 	if result != "even" {
-		t.Eroorf("excepted: even, actual: %s", result)
+		t.Errorf("excepted: even, actual: %s", result)
 	}
 }
